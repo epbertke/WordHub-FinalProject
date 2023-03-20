1e189f664692f0bc5ac391fb2464b58f1f7e2e05
Ellie Bertke <elliebertke@gmail.com>
2023-03-20 17:56:22 -0400

    Refactor CLI welcome and exit messages

--- src/main/java/edu/bsu/cs222/CLI.java
+++ src/main/java/edu/bsu/cs222/CLI.java
@@ -6,12 +6,12 @@ public class CLI {
         System.out.println(LearnTheDictionary.fetchOutputForUser());
         printExitStatement();
     }
-    private static void printWelcomeStatement(){
-        System.out.println("------------------------"+"\n"+ "Learn The Dictionary"+"\n"+"------------------------");
+    private static void printWelcomeStatement() {
+        System.out.println("-".repeat(24)+"\n"+ "Learn The Dictionary"+"\n"+"-".repeat(24));
         System.out.println("If you would like to learn a random word; Enter R"+"\n"+
                 "If you would like to search a specific word; Enter the word");
     }
-    private static void printExitStatement(){
-        System.out.println("\n" + "------------------------" + "\n" + "Re-run program to learn another word." + "\n" + "------------------------" + "\n");
+    private static void printExitStatement() {
+        System.out.println("\n" + "-".repeat(40) + "\n" + "Re-run program to learn another word" + "\n" + "-".repeat(40) + "\n");
     }
 }
