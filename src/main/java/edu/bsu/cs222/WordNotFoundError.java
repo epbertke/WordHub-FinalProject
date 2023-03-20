b2698eaf02eeb1a93dadbd831f338174ee9f5c4f
caleb.reyes <caleb.reyes@bsu.edu>
2023-03-20 16:50:22 -0400

    Added a test for word not found. As well as the error handling for that.

--- src/main/java/edu/bsu/cs222/WordNotFoundError.java
+++ src/main/java/edu/bsu/cs222/WordNotFoundError.java
@@ -1,6 +1,18 @@
 package edu.bsu.cs222;
+
+import java.io.IOException;
+
 public class WordNotFoundError {
     protected static Error throwWordNotFoundError(){
         throw new Error("This word was not found in the dictionary. Re-run program and try again with a new word or different spelling.");
+    }
+    public static String checkForWordNotFoundError(String wordSearch) throws IOException{
+        String definition;
+        try{
+            definition = new DefinitionParser(new APIConnection(wordSearch).getDefinitionsInputStream()).parseForDefinition();
+
+        }catch(Error e){
+            return e.getMessage();
+        }return definition;
     }
 }
--- src/test/java/WordSearchTests.java
+++ src/test/java/WordSearchTests.java
@@ -1,3 +1,4 @@
+import edu.bsu.cs222.WordNotFoundError;
 import edu.bsu.cs222.WordSearch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,5 +18,9 @@ public class WordSearchTests {
     public void wordSearchGivesSynonymsOfWordTest() throws IOException {
         boolean result = new WordSearch("grateful").getSearchedWordInformation()[2].isEmpty();
         Assertions.assertFalse(result);
+    }
+    @Test
+    public void invalidWordSearchGivesErrorTest() throws IOException {
+        Assertions.assertEquals("This word was not found in the dicitonary. Re-run program and try again with a new word or different spelling.", WordNotFoundError.checkForWordNotFoundError("jkfndsajnfdsa"));
     }
 }
