31b70caa3edbf044f0d819b4c71e40a621a968e6
caleb.reyes <caleb.reyes@bsu.edu>
2023-03-20 16:30:18 -0400

    Remove 2 not needed methods from WordNotFoundError

--- src/main/java/edu/bsu/cs222/WordNotFoundError.java
+++ src/main/java/edu/bsu/cs222/WordNotFoundError.java
@@ -3,12 +3,4 @@ public class WordNotFoundError {
     protected static Error throwWordNotFoundError(){
         throw new Error("This word was not found in the dictionary. Re-run program and try again with a new word or different spelling.");
     }
-    protected static Error throwSearchNotProvidedError(){
-        throw new Error("No response was provided. Re-run program and try again.");
-    }
-    protected static boolean checkForUserInput(String userInput){
-        if(userInput.isBlank()){
-            return false;
-        }return true;
-    }
 }
