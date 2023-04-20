edadf23bffca512236b41e6c0aff93fe0fa78d8f
Nathaniel Grigsby <ncgrigsby@bsu.edu>
2023-04-20 13:34:30 -0400

    Added tests for translating Portuguese and Spanish

--- src/test/java/edu/bsu/cs222/ForeignTranslatorTests.java
+++ src/test/java/edu/bsu/cs222/ForeignTranslatorTests.java
@@ -8,6 +8,14 @@ import edu.bsu.cs222.language.translator.translators.german.translators.GermanTo
 import edu.bsu.cs222.language.translator.translators.german.translators.GermanToFrenchTranslator;
 import edu.bsu.cs222.language.translator.translators.german.translators.GermanToPortugueseTranslator;
 import edu.bsu.cs222.language.translator.translators.german.translators.GermanToSpanishTranslator;
+import edu.bsu.cs222.language.translator.translators.portuguese.translators.PortugueseToChineseTranslator;
+import edu.bsu.cs222.language.translator.translators.portuguese.translators.PortugueseToFrenchTranslator;
+import edu.bsu.cs222.language.translator.translators.portuguese.translators.PortugueseToGermanTranslator;
+import edu.bsu.cs222.language.translator.translators.portuguese.translators.PortugueseToSpanishTranslator;
+import edu.bsu.cs222.language.translator.translators.spanish.translators.SpanishToChineseTranslator;
+import edu.bsu.cs222.language.translator.translators.spanish.translators.SpanishToFrenchTranslator;
+import edu.bsu.cs222.language.translator.translators.spanish.translators.SpanishToGermanTranslator;
+import edu.bsu.cs222.language.translator.translators.spanish.translators.SpanishToPortugueseTranslator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
@@ -59,5 +67,37 @@ public class ForeignTranslatorTests {
     @Test
     public void germanToPortugueseTranslatorGivesWordInPortugueseTest() throws IOException, InterruptedException {
         Assertions.assertEquals("tempo", new GermanToPortugueseTranslator("zeit").getTranslatedWordInPortuguese());
+    }
+    @Test
+    public void portugueseToSpanishTranslatorGivesWordInSpanishTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("tiempo", new PortugueseToSpanishTranslator("tempo").getTranslatedWordInSpanish());
+    }
+    @Test
+    public void portugueseToChineseTranslatorGivesWordInChineseTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("时间", new PortugueseToChineseTranslator("tempo").getTranslatedWordInChinese());
+    }
+    @Test
+    public void portugueseToFrenchTranslatorGivesWordInFrenchTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("temps", new PortugueseToFrenchTranslator("tempo").getTranslatedWordInFrench());
+    }
+    @Test
+    public void PortugueseToGermanTranslatorGivesWordInGermanTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("zeit", new PortugueseToGermanTranslator("tempo").getTranslatedWordInGerman());
+    }
+    @Test
+    public void spanishToPortugueseTranslatorGivesWordInPortugueseTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("tempo", new SpanishToPortugueseTranslator("tiempo").getTranslatedWordInPortuguese());
+    }
+    @Test
+    public void spanishToChineseTranslatorGivesWordInChineseTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("时间", new SpanishToChineseTranslator("tiempo").getTranslatedWordInChinese());
+    }
+    @Test
+    public void spanishToFrenchTranslatorGivesWordInFrenchTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("temps", new SpanishToFrenchTranslator("tiempo").getTranslatedWordInFrench());
+    }
+    @Test
+    public void spanishToGermanTranslatorGivesWordInGermanTest() throws IOException, InterruptedException {
+        Assertions.assertEquals("zeit", new SpanishToGermanTranslator("tiempo").getTranslatedWordInGerman());
     }
 }
